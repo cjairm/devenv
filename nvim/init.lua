35c3964cd4c0a1323bd9a6dc69ee8d815bdffee4
Jair Mendez <carlos@hadaelectronics.com>
2024-11-15 21:15:07 -0600

    Going back to tokyo due compatiblities
    See more: https://linovox.com/the-best-color-schemes-for-neovim-nvim/

--- nvim/init.lua
+++ nvim/init.lua
@@ -905,6 +905,23 @@ require('lazy').setup({
     end,
   },
 
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
@@ -1007,8 +1024,6 @@ require('lazy').setup({
 
   'christoomey/vim-tmux-navigator',
 
-  'ellisonleao/gruvbox.nvim',
-
   {
     'kdheepak/lazygit.nvim',
     -- optional for floating window border decoration
@@ -1058,10 +1073,6 @@ vim.g.NERDTreeQuitOnOpen = 1
 
 -- Enable relative number
 vim.o.relativenumber = true
-
--- [[ Theme ]]
-vim.o.background = 'dark' -- or "light" for light mode
-vim.cmd [[colorscheme gruvbox]]
 
 -- [[ NERDTree ]]
 vim.keymap.set('n', '<leader>nt', ':NERDTreeToggle<CR>', { desc = '[N]erdTree [T]oggle' })
